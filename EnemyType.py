d19d5b3a0996d81dd29a3d13f6794b65b8db1162
Elon61 <Elon61@users.noreply.github.com>
2022-08-06 15:16:39 +0300

    Shoots better, and faster

--- EnemyType.py
+++ EnemyType.py
@@ -1,3 +1,4 @@
+from DataStructures import EnemySpriteGroup
 from MovementDir import MovePattern
 from Spriteables import BulletHellSprite
 
@@ -6,4 +7,5 @@ class EnemyType(BulletHellSprite):
 
     def __init__(self, location, sprite, velocity: MovePattern, health=1):
         super().__init__(location, sprite, velocity)
+        EnemySpriteGroup.add(self)
         self.health = health
--- Player.py
+++ Player.py
@@ -6,22 +6,23 @@ import Draw
 from DataStructures import PlayerSpriteGroup
 from MovementDir import MovePattern, PlayerMovementPattern
 from Projectile import Projectile
-from Spriteables import BulletHellSprite
+from Spriteables import BulletHellSprite, out_of_bounds
 
 
 class Player(BulletHellSprite):
     def __init__(self, location, sprite):
         super().__init__(location, sprite, PlayerMovementPattern((1, 1)))
         PlayerSpriteGroup.add(self)
-        self.shot_cd = 60
+        self.shot_cd = 10
         self.curr_cd = 0
 
     def get_location(self):
         return self.location
 
-    # not sure if it is wise to do accelerated movement
-    def action(self, direction, shoot):
-        self.location += direction
+    def action(self, direction, shoot):  # TODO: change the direction implementation to return the dict, and pass that to PlayerMovementPattern via varargs, which will handle the bound checks. this function should probably be update instead of action.
+        newloc = self.location + direction * 10
+        if not out_of_bounds(newloc, self.imsize):
+            self.location += direction * 10
         self.curr_cd += 1
         if shoot and self.curr_cd > self.shot_cd:
             print("powpow")
