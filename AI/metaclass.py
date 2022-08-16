30745e9216c82bb89a1cf9ac8dee67cbbd082691
Elon61 <Elon61@users.noreply.github.com>
2022-08-16 21:43:18 +0300

    Refractory cement

--- AI/metaclass.py
+++ AI/metaclass.py
@@ -1,3 +1,6 @@
+from AI_Interactions import AIInteractions
+
+
 class AIBase:
     pass
 
@@ -8,6 +11,22 @@ def null_heuristic(state, problem=None):
     goal in the provided SearchProblem.  This heuristic is trivial.
     """
     return 0
+
+
+def basic_heuristic(state, problem: AIInteractions = None):
+    """
+    A heuristic function estimates the cost from the current state to the nearest
+    goal in the provided SearchProblem.  This heuristic is trivial.
+    """
+    return problem.get_state_score(state)
+
+
+def basic_heuristic_2(state, problem: AIInteractions = None):
+    """
+    A heuristic function estimates the cost from the current state to the nearest
+    goal in the provided SearchProblem.  This heuristic is trivial.
+    """
+    return problem.get_state_score(state)
 
 
 def find_loc(func, arr, element):
@@ -24,21 +43,20 @@ def find_loc(func, arr, element):
     return l
 
 
-# reverse from min to max (score), make
-def a_star_search(problem, node_search_quota = 10000, heuristic=null_heuristic):
+def a_star_search(problem: AIInteractions, node_search_quota=10000, heuristic=null_heuristic):
     """
-   Search the node that has the lowest combined cost and heuristic first.
-   """
+    Search the node that has the lowest combined cost and heuristic first.
+    """
     # get starting point
-    board_instance: board.Board = problem.get_start_state()
+    board_instance = problem.get_next_start_state()
     queue = problem.get_successors(board_instance)
     # make a visited list, paternity list
     visited = set(x for (x, y, z) in queue)
     father = {}
-    g = lambda x: x[2] + heuristic(x[0], problem)
+    g = lambda x: x[2] + heuristic(x[0], problem)  # x = (curr_state, curr_move, score), where the current state is the state *after* curr_move has been executed, and the score is also after the move.
     i = 0
     while len(queue):
-        curr_state, curr_move, score = queue.pop(0)
+        curr_state, curr_move, score = queue.pop()  # reversed from min to max (score)
         if node_search_quota == i:
             path = []
             while (curr_state, curr_move) in father.keys():
@@ -46,15 +64,12 @@ def a_star_search(problem, node_search_quota = 10000, heuristic=null_heuristic):
                 curr_state, curr_move = father[(curr_state, curr_move)]
             path.insert(0, curr_move)
             return path
-        for (bstate, bmove, reward) in problem.get_successors(curr_state):
+        for (bstate, bmove, reward) in problem.get_successors(curr_state):  # TODO alternatively, modify A* to get the successors of currstate+curr+move instead of currstate being prevstate + currmove.
             if bstate not in visited:
-                queue.insert(
-                    find_loc(g, queue, (bstate, bmove, reward + score)),
-                    (bstate, bmove, reward + score))
+                queue_elem = (bstate, bmove, reward + score)
+                queue.insert(find_loc(g, queue, queue_elem), queue_elem)
                 visited.add(bstate)
                 if (bstate, bmove) not in father:
                     father[(bstate, bmove)] = (curr_state, curr_move)
         i += 1
     return []
-
-
